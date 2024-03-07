fecb7715cc775b8f8fd29316b6269aa402361ff9
Diurno <Diurno@C08PC09.informatica.comercio>
2024-03-07 12:27:10 +0100

    fin

---
+++ src/main/java/clases/Comando.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author Diurno
+ */
+public class Comando {
+    private String comando;
+    private String segundaPalabra;
+}
